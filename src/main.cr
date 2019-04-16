9e18aac2896726aafd6ead4751f253a421ad4ac7
torvim <torvimmm@gmail.com>
2019-04-16 11:02:41 +0000

    command history

--- src/main.cr
+++ src/main.cr
@@ -3,21 +3,22 @@ require "fancyline"
 
 def main()
 
-	loop do
-		fancy = Fancyline.new
-		input = fancy.readline("> ")
+	fancy = Fancyline.new
+
+	while input = fancy.readline("$ ") # Ask the user for input
 		next if input.nil? || input.empty?
 		input = input.split(" ")
 
+		#split input into base and arguments
 		base = input.shift
 		args = input
 
+		#fork process and transorm into base and arguments
 		process = Process.fork {
-
 			Process.exec base, args
-			
 		}
 
+		#wait for process to end
 		process.wait()
 	end
 end
