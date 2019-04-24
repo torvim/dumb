595797571be2b18da435df011315fe33159e1cae
torvim <31416064+torvim@users.noreply.github.com>
2019-04-24 12:24:53 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+git clone https://github.com/torvim/nu
+cd nu
+
 if
 	shards install
 	sudo crystal build src/main.cr -o /usr/bin/nu ; then
@@ -8,3 +11,5 @@ else
     echo "Unsuccessfully compiled and installed"
 fi
 
+cd ../
+rm nu -rf
